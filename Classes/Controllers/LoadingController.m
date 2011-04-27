7b35ee9b1bd68f0fee624a16d9d1d154daa135a4
news:yc developer <newsyc@grantpaul.com>
2011-04-27 00:33:56 -0700

    Fix width of spinner.

--- Classes/Controllers/LoadingController.m
+++ Classes/Controllers/LoadingController.m
@@ -119,7 +119,7 @@
     [errorLabel setTextAlignment:UITextAlignmentCenter];
     
     actionItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(actionTapped)];
-    loadingItem = [[ActivityIndicatorItem alloc] initWithSize:CGSizeMake(25.0f, kActivityIndicatorItemStandardSize.height)];
+    loadingItem = [[ActivityIndicatorItem alloc] initWithSize:CGSizeMake(27.0f, kActivityIndicatorItemStandardSize.height)];
 }
 
 - (void)viewDidLoad {
