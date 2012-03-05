cfb3fd6d686e871cb7645df016e7590b352d37bf
news:yc developer <newsyc@grantpaul.com>
2012-03-04 22:58:11 -0800

    Implement a top-down renderer. This causes issues with conflicts, as higher element properties are applied /after/ those of subelements. Therefore, it is not the default renderer. It also may be kinda slow.

--- Classes/HNKit/HNEntryBodyRenderer.m
+++ Classes/HNKit/HNEntryBodyRenderer.m
@@ -49,45 +49,95 @@
     CGColorRef colorBody = [[UIColor blackColor] CGColor];
     CGColorRef colorLink = [[UIColor blueColor] CGColor];
     
-    __block NSMutableAttributedString *bodyAttributed = [[NSMutableAttributedString alloc] init];
-    __block NSMutableDictionary *currentAttributes = [NSMutableDictionary dictionary];
-        
-    void(^formatBody)(NSMutableDictionary *, XMLElement *) = ^(NSMutableDictionary *attributes, XMLElement *element) {
+    __block BOOL preserveWhitespace = NO;
+    __block NSMutableAttributedString *(^formatElementChildren)(XMLElement *) = nil;
+    
+    NSAttributedString *(^formatText)(XMLElement *) = ^NSAttributedString *(XMLElement *element) {
+        NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
         [attributes setObject:(id) colorBody forKey:(NSString *) kCTForegroundColorAttributeName];
         [attributes setObject:(id) fontBody forKey:(NSString *) kCTFontAttributeName];
+        
+        NSString *content = [element content];
+        
+        // strip out whitespace not in <code> when 
+        if (!preserveWhitespace) {
+            while ([content rangeOfString:@"  "].location != NSNotFound) {
+                content = [content stringByReplacingOccurrencesOfString:@"  " withString:@" "];
+            }
+         
+            content = [content stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+        }
+        
+        NSAttributedString *contentAttributed = [[NSAttributedString alloc] initWithString:content attributes:attributes];
+        return [contentAttributed autorelease];
     };
     
-    void(^formatFont)(NSMutableDictionary *, XMLElement *) = ^(NSMutableDictionary *attributes, XMLElement *element) {
+    NSAttributedString *(^formatFont)(XMLElement *) = ^NSAttributedString *(XMLElement *element) {
+        NSMutableAttributedString *attributedElement = formatElementChildren(element);
+
         NSString *colorText = [element attributeWithName:@"color"];
         CGColorRef color = [[self colorFromHexString:colorText] CGColor];
-        
+        NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
         [attributes setObject:(id) color forKey:(NSString *) kCTForegroundColorAttributeName];
-    };
-    
-    void(^formatCode)(NSMutableDictionary *, XMLElement *) = ^(NSMutableDictionary *attributes, XMLElement *element) {
-        [attributes setObject:(id) fontCode forKey:(NSString *) kCTFontAttributeName];
-        [attributes setObject:(id) kCFBooleanTrue forKey:@"PreserveWhitepace"];
-    };
-    
-    void(^formatItalic)(NSMutableDictionary *, XMLElement *) = ^(NSMutableDictionary *attributes, XMLElement *element) {
-        [attributes setObject:(id) fontItalic forKey:(NSString *) kCTFontAttributeName];
-    };
-    
-    void(^formatLink)(NSMutableDictionary *, XMLElement *) = ^(NSMutableDictionary *attributes, XMLElement *element) {
-        NSString *href = [element attributeWithName:@"href"];
+        [attributedElement addAttributes:attributes range:NSMakeRange(0, [attributedElement length])];
         
+        return attributedElement;
+    };
+    
+    NSAttributedString *(^formatCode)(XMLElement *) = ^NSAttributedString *(XMLElement *element) {
+        preserveWhitespace = YES;
+        NSMutableAttributedString *attributedElement = formatElementChildren(element);
+        preserveWhitespace = NO;
+        
+        NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+        [attributes setObject:(id) fontCode forKey:(NSString *) kCTFontAttributeName];
+        [attributedElement addAttributes:attributes range:NSMakeRange(0, [attributedElement length])];
+
+        return attributedElement;
+    };
+    
+    NSAttributedString *(^formatItalic)(XMLElement *) = ^NSAttributedString *(XMLElement *element) {
+        NSMutableAttributedString *attributedElement = formatElementChildren(element);
+        
+        NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+        [attributes setObject:(id) fontItalic forKey:(NSString *) kCTFontAttributeName];
+        [attributedElement addAttributes:attributes range:NSMakeRange(0, [attributedElement length])];
+        
+        return attributedElement;
+    };
+    
+    NSAttributedString *(^formatLink)(XMLElement *) = ^NSAttributedString *(XMLElement *element) {
+        NSMutableAttributedString *attributedElement = formatElementChildren(element);
+        
+        NSString *href = [element attributeWithName:@"href"];
+        NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
         [attributes setObject:(id) colorLink forKey:(NSString *) kCTForegroundColorAttributeName];
         if (href != nil) [attributes setObject:href forKey:@"LinkDestination"];
+        [attributedElement addAttributes:attributes range:NSMakeRange(0, [attributedElement length])];
+        
+        return attributedElement;
     };
 
-    void(^formatParagraph)(NSMutableDictionary *, XMLElement *) = ^(NSMutableDictionary *attributes, XMLElement *element) {
-        NSAttributedString *childString = [[NSAttributedString alloc] initWithString:@"\n\n" attributes:nil];
-        [bodyAttributed appendAttributedString:[childString autorelease]];
+    NSAttributedString *(^formatParagraph)(XMLElement *) = ^NSAttributedString *(XMLElement *element) {
+        NSMutableAttributedString *attributedElement = [[[NSMutableAttributedString alloc] init] autorelease];
+        
+        NSAttributedString *paragraphAttributed = [[NSAttributedString alloc] initWithString:@"\n\n" attributes:nil];
+        [attributedElement appendAttributedString:paragraphAttributed];
+        [paragraphAttributed release];
+        
+        [attributedElement appendAttributedString:formatElementChildren(element)];
+        
+        return attributedElement;
     };
     
-    void (^formatNewline)(NSMutableDictionary *, XMLElement *) = ^(NSMutableDictionary *attributes, XMLElement *element) {
-        NSAttributedString *childString = [[NSAttributedString alloc] initWithString:@"\n" attributes:nil];
-        [bodyAttributed appendAttributedString:[childString autorelease]];
+    NSAttributedString *(^formatNewline)(XMLElement *) = ^NSAttributedString *(XMLElement *element) {
+        NSMutableAttributedString *attributedElement = formatElementChildren(element);
+        
+        NSAttributedString *newlineAttributed = [[NSAttributedString alloc] initWithString:@"\n" attributes:nil];
+        [attributedElement appendAttributedString:newlineAttributed];
+        [newlineAttributed release];
+        
+        return attributedElement;
     };
     
     NSDictionary *tagActions = [NSDictionary dictionaryWithObjectsAndKeys:
@@ -97,50 +147,46 @@
         [[formatLink copy] autorelease], @"a",
         [[formatFont copy] autorelease], @"font",
         [[formatNewline copy] autorelease], @"br",
-        [[formatBody copy] autorelease], @"body",
     nil];
 
-    __block void(^formatChildren)(XMLElement *) = ^(XMLElement *element) {
-        for (XMLElement *child in [element children]) {
-            if (![child isTextNode]) {
-                NSMutableDictionary *savedAttributes = [[currentAttributes mutableCopy] autorelease];
-                
-                NSAttributedString *(^formatAction)(NSMutableDictionary *, XMLElement *element) = [tagActions objectForKey:[child tagName]];
-                if (formatAction != NULL) formatAction(currentAttributes, child);
-                
-                formatChildren(child);
-                
-                currentAttributes = savedAttributes;
-            } else {
-                NSString *content = [child content];
-                
-                // strip out whitespace not in <code> when 
-                if (![[currentAttributes objectForKey:@"PreserveWhitepace"] boolValue]) {
-                    while ([content rangeOfString:@"  "].location != NSNotFound) {
-                        content = [content stringByReplacingOccurrencesOfString:@"  " withString:@" "];
-                    }
-                    
-                    content = [content stringByReplacingOccurrencesOfString:@"\n" withString:@""];
-                }
-                
-                NSAttributedString *childString = [[NSAttributedString alloc] initWithString:content attributes:currentAttributes];
-                [bodyAttributed appendAttributedString:[childString autorelease]];
-            }
+    __block NSAttributedString *(^formatChildren)(XMLElement *) = ^NSAttributedString *(XMLElement *element) {
+        NSMutableAttributedString *formatAttributed = [[[NSMutableAttributedString alloc] init] autorelease];
+        
+        NSAttributedString *(^formatAction)(XMLElement *element) = [tagActions objectForKey:[element tagName]];
+        
+        if (formatAction != NULL) {
+            [formatAttributed appendAttributedString:formatAction(element)];
+        } else {
+            [formatAttributed appendAttributedString:formatElementChildren(element)];
         }
+        
+        return formatAttributed;
     };
     
-    // ensure body has a root element
-    body = [NSString stringWithFormat:@"<body>%@</body>", body];
+    formatElementChildren = ^NSMutableAttributedString *(XMLElement *element) {
+        NSMutableAttributedString *attributedElement = [[[NSMutableAttributedString alloc] init] autorelease];
+        
+        for (XMLElement *child in [element children]) {
+            if ([child isTextNode]) {
+                [attributedElement appendAttributedString:formatText(child)];
+
+            } else {
+                [attributedElement appendAttributedString:formatChildren(child)];
+            }
+        }
+        
+        return attributedElement;
+    };
     
     XMLDocument *xml = [[XMLDocument alloc] initWithHTMLData:[body dataUsingEncoding:NSUTF8StringEncoding]];
-    formatChildren([xml firstElementMatchingPath:@"/"]);
+    NSAttributedString *bodyAttributed = formatChildren([xml firstElementMatchingPath:@"/"]);
     [xml release];
     
     CFRelease(fontBody);
     CFRelease(fontCode);
     CFRelease(fontItalic);
     
-    return [bodyAttributed autorelease];
+    return bodyAttributed;
 }
 
 - (CGSize)sizeForWidth:(CGFloat)width {
