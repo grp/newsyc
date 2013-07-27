2c3316530d25d46038526902079fde1c3c643516
Hank Jacobs <hank8903@gmail.com>
2013-07-27 15:18:08 -0700

    Fix Instapaper Root API issue
    Fixed issue that was causing login request to return a 403.

--- Classes/Instapaper/InstapaperAPI.h
+++ Classes/Instapaper/InstapaperAPI.h
@@ -8,6 +8,6 @@
 
 #import <HNKit/NSString+URLEncoding.h>
 
-#define kInstapaperAPIRootURL [NSURL URLWithString:@"https://instapaper.com/api/"]
+#define kInstapaperAPIRootURL [NSURL URLWithString:@"https://www.instapaper.com/api/"]
 #define kInstapaperAPIAuthenticationURL [NSURL URLWithString:[[kInstapaperAPIRootURL absoluteString] stringByAppendingString:@"authenticate"]]
 #define kInstapaperAPIAddItemURL [NSURL URLWithString:[[kInstapaperAPIRootURL absoluteString] stringByAppendingString:@"add"]]
