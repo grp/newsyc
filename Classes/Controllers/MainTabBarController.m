67cc18a9c7805ba4615edb33205edee6d8fb4f4e
Grant Paul <git@grantpaul.com>
2013-11-29 21:59:57 -0800

    Update home icon for iOS 7. Last non-operational todo list item.

--- Classes/Controllers/MainTabBarController.m
+++ Classes/Controllers/MainTabBarController.m
@@ -56,7 +56,7 @@
         HNEntryList *homeList = [HNEntryList session:session entryListWithIdentifier:kHNEntryListIdentifierSubmissions];
         home = [[[SubmissionListController alloc] initWithSource:homeList] autorelease];
         [home setTitle:@"Hacker News"];
-        [home setTabBarItem:[[[UITabBarItem alloc] initWithTitle:@"Home" image:[UIImage imageNamed:@"home.png"] tag:0] autorelease]];
+        [home setTabBarItem:[self _tabBarItemWithTitle:@"Home" imageName:@"home"]];
         
         HNEntryList *newList = [HNEntryList session:session entryListWithIdentifier:kHNEntryListIdentifierNewSubmissions];
         latest = [[[SubmissionListController alloc] initWithSource:newList] autorelease];
